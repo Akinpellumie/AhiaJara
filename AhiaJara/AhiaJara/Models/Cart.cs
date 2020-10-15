5e5debf9f28b0ae382ff4f50296782fb334f17b9
tomiczilla@gmail.com <tomiczilla@gmail.com>
2020-10-15 04:05:45 -0700

    New api consumed add to cart, view cart etc

--- AhiaJara/AhiaJara/Models/Cart.cs
+++ AhiaJara/AhiaJara/Models/Cart.cs
@@ -12,7 +12,12 @@ namespace AhiaJara.Models
     {
         public string productId { get; set; }
         public string userId { get; set; }
-        public string quantitySelected { get; set; }
+        public int quantitySelected { get; set; }
+        public string productName { get; set; }
+        public string productImgUrl { get; set; }
+        public string productPrice { get; set; }
+        public string productCategory { get; set; }
+
     }
 
     public class CartCount
@@ -20,73 +25,55 @@ namespace AhiaJara.Models
         public int cartcount { get; set; }
     }
 
-    public class CartDetails
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Cart
     {
         public string productId { get; set; }
         public string userId { get; set; }
         public int quantitySelected { get; set; }
+
+        public string productName { get; set; }
+        public string productImgUrl { get; set; }
+        public string productPrice { get; set; }
+        public string productCategory { get; set; }
         public DateTime createdAt { get; set; }
         public DateTime updatedAt { get; set; }
         public string id { get; set; }
-        public ProductModel productDetails { get; set; }
-
-    }
-
-    public class CartItemDetails
-    {
-        public string productId { get; set; }
-        public string userId { get; set; }
-        public int quantitySelected { get; set; }
-        public string id { get; set; }
-
-        public string name { get; set; }
-        public string imgUrl { get; set; }
-        public int quantityAvailable { get; set; }
-        //[JsonProperty("price")]
-        public string price { get; set; }
-        public string category { get; set; }
-        public string description { get; set; }
-        public DateTime createdAt { get; set; }
-        public DateTime updatedAt { get; set; }
-
-        public ImageSource ProductImage
-        {
-
-            get
-            {
-                var source = new Uri(imgUrl);
-                //var source = new Uri("http://192.168.1.118:5000/images/uploaded_images/" + Image);
-
-                return source;
-            }
-            set { ProductImage = value; }
-        }
 
         public string Price
         {
             get
             {
 
-                var pel = Math.Round(Convert.ToDouble(this.price), 2).ToString("C", CultureInfo.GetCultureInfo("en-us")).Replace("$", "NGN ");
+                var pel = Math.Round(Convert.ToDouble(this.productPrice), 2).ToString("C", CultureInfo.GetCultureInfo("en-us")).Replace("$", "NGN ");
                 return pel;
             }
             set {; }
         }
 
+        public string qtySelected
+        {
+            get
+            {
+                string quantity = this.quantitySelected.ToString();
+                var quantityText = "Quantity: " + quantity;
+                return quantityText;
+            }
+            set { qtySelected = value; }
+        }
+        public ImageSource ProductImage
+        {
+
+            get
+            {
+                var source = new Uri(productImgUrl);
+                //var source = new Uri("http://192.168.1.118:5000/images/uploaded_images/" + Image);
+
+                return source;
+            }
+            set { ProductImage = value; }
+        }
     }
 
-
-    public class CartList
-    {
-        public CartDetails[] cartDetails { get; set; }
-       // public CartDetails cartDetails { get; set; }
-        //public ProductModel productDetails { get; set; }
-    }
-
-    //public class CartList
-    //{
-    //    public List<MyArray> MyArray { get; set; }
-    //    ////public Dictionary<string, MyArray>MyArray  { get; set; }
-    //}
 
 }
